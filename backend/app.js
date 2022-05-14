15ecb385ebb70856b84a5660389f0759ac4c850c
soulrrrrr <chialun100@gmail.com>
2022-05-14 12:30:39 +0800

    body parser

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+const bp = require('body-parser')
+app.use(bp.json());
 
 app.use('/api', cors(), indexRouter);
 // app.use('/users', usersRouter);
